3cc16f7d14b42f27084fd8f6130b4a48443c6828
Tseng, Ming Yu <qazwsx9006@hotmail.com>
2019-11-04 20:54:50 +0800

    add socketio & jest

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
+const Server = require("socket.io");
 const config = require("config");
 const Koa = require("koa");
 const app = new Koa();
+let io;
 
-// response
+// response;
 app.use(ctx => {
   ctx.body = "Hello Koa";
 });
 
-app.listen(config.socket.port);
+const server = app.listen(config.socket.port, async () => {
+  console.log(`Server listening on port ${config.socket.port}`);
+});
+
+io = new Server(server);
+
+io.on("connection", function(socket) {
+  console.log("a user connected");
+});
---
+++ test/index.test.js
@@ -0,0 +1,17 @@
+const clientSocket = require("socket.io-client");
+const server = require("../index");
+jest.setTimeout(10000);
+
+let socket;
+
+describe("socket jwt token auth success", () => {
+  beforeAll(async done => {
+    const socketUrl = "http://localhost:3000";
+    socket = clientSocket(socketUrl);
+    done();
+  });
+
+  test("should connect success", function(done) {
+    socket.on("connect", done);
+  });
+});
